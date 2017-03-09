1418e92e50d0594ff19e04d04a840a019e4f2e0e
jiangwei37 <jiangwei5@jd.com>
2017-03-09 14:31:45 +0800

    jw

--- src/components/address/UserAddress.vue
+++ src/components/address/UserAddress.vue
@@ -3,7 +3,7 @@
   <div class='address-main-div'>
     <div class='main-header border-b' v-bind:class='{none:showHeader}'>
       <h2 v-on:click='changeCity' class='main-header-city' v-bind:class='{none:showHeaderCity}'>{{defaultCityName}}</h2>
-      <input v-on:click='test' ref='inputCity' class='main-header-search' v-bind:class='{defaultWidth:showHeaderCity}' maxlength='50' type='text' placeholder='选择城市、小区、写字楼、学校' />
+      <input v-on:click='test' v-on:input='inputAddress' ref='inputCity' class='main-header-search' v-bind:class='{defaultWidth:showHeaderCity}' maxlength='50' type='text' placeholder='选择城市、小区、写字楼、学校' />
     </div>
     <div class='main-list-content' v-bind:class='{none:showAddressList}'>
       <div class='main-locate'>
@@ -31,12 +31,10 @@
         </div>
       </div>
         <div class='main-history-content' v-bind:class='{none:showHistory}'>
-          <HistoryAddress :historyData='historyData'></HistoryAddress>
+          <HistoryAddress :historyData='historyData' v-on:clearHistory='clearHistory'></HistoryAddress>
         </div>
-        <div class='main-poi-content none'>
-            <ul>
-
-            </ul>
+        <div class='main-poi-content' v-bind:class='{none:showPoi}'>
+          <PoiView :poiAddress='poiAddress'></PoiView>
         </div>
     <div class='address-city-div' v-bind:class='{none:showCityName}'>
         <h2 class='address-city-title'><span>已开通城市</span></h2>
@@ -51,12 +49,15 @@
 <script>
 import HistoryAddress from './historyAddress.vue'
 import CityView from './CityView.vue'
+import PoiView from './PoiAddress.vue'
+
+let poiAddress
+let historyData
 
 export default{
   props: {
     // test1:String,
     items: Array,
-    historyData: Array,
     city: Array
   },
   data: function () {
@@ -66,7 +67,10 @@ export default{
       'showHistory': true,
       'showHeaderCity': true,
       'showAddressList': false,
-      'defaultCityName': '北京市'
+      'showPoi': true,
+      'defaultCityName': '北京市',
+      'poiAddress': poiAddress,
+      'historyData': historyData
     }
   },
   computed: {
@@ -78,15 +82,17 @@ export default{
   },
   methods: {
     test: function (e) {
-      this.showHeaderCity = false
-      this.showAddressList = true
-      this.showHistory = false
-      // console.log(this.$refs.inputCity)
+      if (!this.$refs.inputCity.value) {
+        this.showHeaderCity = false
+        this.showAddressList = true
+        this.getHistory()
+      }
     },
     changeCity: function (e) {
       this.showHeader = true
       this.showHistory = true
       this.showCityName = false
+      this.showAddressList = true
     },
     chooseAddress: function (e) {
       // 选择地址之后返回上个页面
@@ -98,11 +104,51 @@ export default{
       this.showCityName = true
       this.showAddressList = false
       this.defaultCityName = cityName
+    },
+    inputAddress: function (e) {
+      let inputValue = this.$refs.inputCity.value
+      if (inputValue) {
+        let data = {
+          functionId: 'address/search',
+          body: {
+            region: this.defaultCityName,
+            key: inputValue
+          }
+        }
+        this.$http.get('/client', {params: data}).then(response => {
+          this.showPoi = false
+          this.showHistory = true
+          this.poiAddress = response.body.result
+        }, reposonse => {
+
+        })
+      } else {
+        this.showPoi = true
+        this.showHistory = false
+      }
+    },
+    clearHistory: function () {
+      this.showHistory = true
+    },
+    getHistory: function () {
+      let data = {
+        functionId: 'local/getSearchInfos',
+        body: {}
+      }
+      this.$http.get('/client', {params: data}).then(response => {
+        this.historyData = response.body.result
+        if (this.historyData && this.historyData.length > 0) {
+          this.showHistory = false
+        }
+      }, reposonse => {
+
+      })
     }
   },
   components: {
     HistoryAddress,
-    CityView
+    CityView,
+    PoiView
   }
 }
 </script>
