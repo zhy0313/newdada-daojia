6b698a1020c09fb10181a2b604e19506fb13be81
Ivan <wcy5211678@163.com>
2017-03-14 18:34:51 +0800

    reset scss

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,3 @@ export default {
   name: 'app'
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
-  color: #2c3e50;
-  background: #f4f4f4;
-  box-sizing: content-box;
-}
-</style>
