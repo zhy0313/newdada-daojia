6bb61cdc5210bcf233e8b0d71d498acde406521d
zhanglei03 <zhanglei03@imdada.cn>
2017-03-17 00:05:35 +0800

    cashier

--- src/views/Cashier/CashierView.vue
+++ src/views/Cashier/CashierView.vue
@@ -1,43 +1,31 @@
 <template lang="html">
-  <div class="pay-wrapper">
-      <div class="pay-header">
-          <div class="pay-last-time-wrapper border-l border-r">
-              <span class="last-time-title">剩余支付时间</span><span class="pay-last-time"><countDown :countDownTime="countDownTime"></countDown></span>
-          </div>
-          <div class="pay-store">
-              <i class="store-top-bg"></i>
-              <i class="store-left-bg"></i>
-              <i class="store-right-bg"></i>
-              <i class="store-bottom-bg"></i>
-              <span class="store-name">{{stationName}}</span>
-              <span class="store-account">￥<i>{{amount}}</i></span>
-          </div>
+  <div class="pay-container">
+    <div class="pay-header">
+      <div class="pay-time">
+        <p>支付剩余时间</p>
+        <h1>14:48</h1>
       </div>
-      <p class="pay-title border-b">请选择支付方式</p>
+      <div class="pay-store-amount">
+        <span class="pay-station">{{stationName}}</span><span class="pay-amount">¥{{amount}}</span>
+      </div>
+      <p class="pay-store-amount-bottom"></p>
+    </div>
+    <div class="pay-type">
+      <p class="pay-title">请选择支付方式</p>
       <ul>
         <template v-for="(item, index) in payWays">
-          <li :class="item.payWay == 10 ? 'wx' : item.payWay == 20 ? 'jd' : item.payWay == 30 ? 'bt' : item.payWay == 31 ? 'nbt' : item.payWay == 40 ? 'zfb' : item.payWay == 70 ? 'dai' :''" class="border-b">
-            <p class="pay-type">{{item.payName}}<span v-show="item.extCopy">({{item.extCopy}})</span></p>
-            <p class="pay-desc">{{item.defaultCopy}}</p>
-            <p class="pay-desc" :class="item.activityCopy ? 'actvity-desc' : ''">{{item.activityCopy}}</p>
+          <li :class="item.payWay == 10 ? 'wx' : item.payWay == 20 ? 'jd' : item.payWay == 30 ? 'bt' : item.payWay == 31 ? 'nbt' : item.payWay == 40 ? 'zfb' : item.payWay == 70 ? 'dai' :''">
+            <p class="pay-name">{{item.payName}}<span v-show="item.extCopy">({{item.extCopy}})</span></p>
+            <p class="pay-default-desc">{{item.defaultCopy}}</p>
+            <!-- <p class="pay-desc" :class="item.activityCopy ? 'actvity-desc' : ''">{{item.activityCopy}}</p> -->
             <i :class="payCheckedClass" @click="changePayType"></i>
           </li>
       </template>
       </ul>
-      <p class="dj-pay-desc"><span>马上支付，可领取分享红包。</span></p>
-      <p class="pay-button">
-          <button isSending="no">确认支付<i>￥</i><em>{{amount}}</em></button>
-      </p>
-      <p class="pay-button-confirm none">
-          <button class="finish">已完成支付</button>
-          <span></span>
-          <button class="refresh" isSending="no">重新支付</button>
-      </p>
+    </div>
+    <p class="pay-hb">马上支付 ，可领取分享红包</p>
+    <a href="javascript:;" class="btn-pay">确认支付<em>¥</em><span>{{amount}}</span></a>
   </div>
-  <!-- <div class="daipay-masker">
-      <img src="http://teststatic.jd.com/daojia/new/images/pay/share_guide.png" class="daipay-share-guide"/>
-      <img src="http://teststatic.jd.com/daojia/new/images/pay/btn_know.png" class="daipay-share-know"/>
-  </div> -->
 </template>
 
 <script>
