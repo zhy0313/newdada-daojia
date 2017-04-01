ee95469b49ff9467f94f404558f633c5dc4f54c2
Ivan <wcy5211678@163.com>
2017-04-01 10:11:46 +0800

    minicart promisePrice style

--- src/components/cart/MiniCart.vue
+++ src/components/cart/MiniCart.vue
@@ -65,6 +65,31 @@
                   }]
                 },
                 {
+                  basePrice : "10",
+                  cartNum : 15,
+                  checkType : 0,
+                  imageUrl : "https://img10.360buyimg.com/n7//jfs/t2686/129/1178913568/131076/548b8113/5736c5d7Nd761b5ad.jpg",
+                  price : "6",
+                  promotePrice : {
+                    basicNum : 11,
+                    basicPrice : "10",
+                    basicTag: {iconText: "原价", type: "7", colorCode: "cacaca"},
+                    promoteNum : 4,
+                    promotePrice : "6",
+                    promoteTag : {iconText: "秒杀", type: "4", colorCode: "ff5c5d"},
+                    promotionTip : "秒杀商品仅剩4件",
+                    skuId : "2002355070",
+                    skuName : "益補健 维生素C含片（桔子味）20片/个",
+                    skuState : 1
+                  },
+                  promotionTip : "秒杀商品仅剩4件",
+                  skuId : "2002355070",
+                  skuName : "益補健 维生素C含片（桔子味）20片/个",
+                  skuState : 1,
+                  tags : [{iconText: "秒杀", type: "4", colorCode: "ff5c5d"}],
+                  weight : "4kg"
+                },
+                {
                   basePrice : "14.9",
                   cartNum : 1,
                   checkType : 0,
