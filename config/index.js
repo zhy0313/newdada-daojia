27a274847edcfd243561d7478e85202380fffd14
Ivan <wcy5211678@163.com>
2017-04-06 16:56:35 +0800

    user

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '//static-o2o.360buyimg.com/daojia/new/images/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -26,7 +26,7 @@ module.exports = {
     port: 8099, // 代理端口
     autoOpenBrowser: true, // 自动打开浏览器
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '//static-o2o.360buyimg.com/daojia/new/images/',
     // 配置代理
     proxyTable: {
       '/client': {
--- src/views/tabBar/MyView.vue
+++ src/views/tabBar/MyView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="">
-    <Loader></Loader>
+    <Loader v-if="loading"></Loader>
+    <UserCover :userInfo="userInfo"></UserCover>
     <FooterNav :activeKey="4"></FooterNav>
   </div>
 
@@ -9,11 +10,39 @@
 <script>
   import FooterNav from '@/components/FooterNav'
   import Loader from '@/components/Loader'
+  import UserCover from '@/components/user/UserCover'
 
   export default {
     components: {
       FooterNav,
-      Loader
+      Loader,
+      UserCover
+    },
+    data () {
+      return {
+        loading: true,
+        userInfo: {}
+      }
+    },
+    created () {
+      this.$postAPI({
+        // functionId: 'login/testLogin'
+        functionId: 'user/getUserInfo',
+        body: { }
+      }).then((response) => {
+        if (response.body.code === '0') {
+          console.log('接口成功')
+          console.log('response', response.result)
+          this.userInfo = response.result
+        } else {
+          console.log('接口失败', response.body)
+        }
+        this.loading = false
+        // this.$loading.toggle()
+        this.$toast({message: response.body.msg, position: 'center'})
+      }, (err) => {
+        console.log('err', err)
+      })
     }
   }
 </script>
