5233f5a78243ba688503789f72080c97859feb5f
Ivan <wcy5211678@163.com>
2017-04-07 12:08:51 +0800

    user entrance

---
+++ src/components/user/UserEntrance.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="user-entrance-container">
+    <div v-for="entrance in entranceList" class="entrance-block">
+        <router-link v-for="item in entrance" :class="item.type" path="item.type">{{item.text}}</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      entranceList: [
+        [
+          {text: '我的关注', type: 'focus'},
+          {text: '我的评价', type: 'comments'},
+          {text: '管理地址', type: 'address'},
+          {text: '绑定手机号', type: 'bind'}
+        ],
+        [
+          {text: '客服与反馈', type: 'service'},
+          {text: '下载京东到家', type: 'download'},
+          {text: '达达配送员招募', type: 'recruit'},
+          {text: '分享有礼', type: 'share'},
+          {text: '用户调研', type: 'research'},
+          {text: '帮助中心', type: 'help'}
+        ],
+        [
+          {text: '关于我们', type: 'about'}
+        ]
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .user-entrance-container {
+    /*功能区块*/
+    .entrance-block {
+      padding-left: 30rpx;
+      margin-bottom: 20rpx;
+      background: #fff;
+    }
+
+    a{
+      height: 90rpx;
+      line-height: 90rpx;
+      border-bottom: 1rpx solid #e8e8e8;
+      position: relative;
+      padding-left: 55rpx;
+    }
+
+    a:last-of-type{
+      border-bottom: none;
+    }
+
+    a::after {
+      content: '';
+      position: absolute;
+      right: 0;
+      width: 44px;
+      margin-left: 4px;
+      height: 40px;
+      background: url(https://static-o2o.360buyimg.com/daojia/new/images/icon/pdj_2_sprites.png) no-repeat -80px -264px;
+      background-size: 320px auto;
+    }
+    view::before {
+      content: '';
+      position: absolute;
+      left: 4px;
+      top: -2px;
+      width: 26px;
+      height: 40px;
+      background: url(https://static-o2o.360buyimg.com/daojia/new/images/icon/pdj_2_sprites.png) no-repeat;
+      background-size: 320px auto;
+    }
+
+    .focus::before{
+      background: url(https://static-o2o.360buyimg.com/daojia/new/images/store/focus.png) -40px 14px no-repeat;
+      background-size: 58px auto;
+      left: 3px;
+    }
+
+    .address::before{
+      background-position: -191px -44px;
+    }
+    .feedback::before{
+      background: url(http://storage.jd.com/wxapp/icon_feedback.png) no-repeat;
+      background-size: 17px auto;
+      left: 3px;
+      top: 14px;
+    }
+    .store::before{
+      background-position: -189px -132px;
+    }
+    .counpon::before{
+      background-position: -145px -42px;
+    }
+    .download::before{
+      background-position: -101px -131px;
+    }
+    .recruit::before{
+      background-position: -189px -176px;
+    }
+    .share::before{
+      background: url(https://static-o2o.360buyimg.com/daojia/new/images/user/invite.png) no-repeat;
+      background-size: 70% auto;
+      top: 10px;
+    }
+    .research::before{
+      background: url(https://static-o2o.360buyimg.com/daojia/new/images/user/research.png) 0 center no-repeat;
+      background-size: 15px 17px;
+    }
+    /*.feedback::before{
+      background: url(https://static-o2o.360buyimg.com/daojia/new/images/user/icon_list_help.png) no-repeat;
+      background-size: 17px;
+      top: 10px;
+    }*/
+    .about::before{
+      background-position: -189px -220px;
+    }
+  }
+</style>
