c7b629f9627627f2a7ebc633198d35e153574656
Ivan <wcy5211678@163.com>
2017-04-11 15:28:29 +0800

    util

--- src/store/uitl/storage.js
+++ src/store/uitl/storage.js
@@ -1,2 +1,37 @@
-export const sessionStorage = {}
-export const localStorage = {}
+
+const store = (type) => {
+  let data = {}
+  let storeEnabled = false
+
+  try {
+    type.setItem('a', 'a')
+    storeEnabled = true
+  } catch (e) { }
+
+  return {
+    getItem (key) {
+      if (storeEnabled) {
+        return JSON.parse(type.getItem(key))
+      } else {
+        return data[key]
+      }
+    },
+    setItem (key, value) {
+      if (storeEnabled) {
+        type.setItem(key, JSON.stringify(value))
+      } else {
+        data[key] = JSON.stringify(value)
+      }
+    },
+    removeItem (key) {
+      if (storeEnabled) {
+        type.removeItem(key)
+      } else {
+        delete data[key]
+      }
+    }
+  }
+}
+
+export const sessionStorage = store(window.sessionStorage)
+export const localStorage = store(window.localStorage)
