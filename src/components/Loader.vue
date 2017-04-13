276789166671bc5d5363f6c4e7472869279810d3
Ivan <wcy5211678@163.com>
2017-04-13 15:11:13 +0800

    loader

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -24,32 +24,35 @@ export default {
     stroke: #673AB7;
     stroke-width: 3px;
     transform-origin: 50%;
-    animation: line 1.6s cubic-bezier(0.4, 0, 0.2, 1) infinite, rotate 1.6s linear infinite;
+    animation: line .2s cubic-bezier(0.4, 0, 0.2, 1) infinite, rotate .2s linear infinite;
     }
 
     @keyframes rotate {
-    from {
-      -webkit-transform: rotate(0);
-              transform: rotate(0); }
-    to {
-      -webkit-transform: rotate(450deg);
-              transform: rotate(450deg);
+      from {
+        -webkit-transform: rotate(0);
+                transform: rotate(0);
+      }
+      to {
+        -webkit-transform: rotate(450deg);
+                transform: rotate(450deg);
       }
     }
 
     @keyframes line {
-    0% {
-      stroke-dasharray: 2, 85.964;
-      -webkit-transform: rotate(0);
-              transform: rotate(0); }
-    50% {
-      stroke-dasharray: 65.973, 21.9911;
-      stroke-dashoffset: 0; }
-    100% {
-      stroke-dasharray: 2, 85.964;
-      stroke-dashoffset: -65.973;
-      -webkit-transform: rotate(90deg);
-              transform: rotate(90deg);
+      0% {
+        stroke-dasharray: 2, 85.964;
+        -webkit-transform: rotate(0);
+                transform: rotate(0);
+      }
+      50% {
+        stroke-dasharray: 65.973, 21.9911;
+        stroke-dashoffset: 0;
+      }
+      100% {
+        stroke-dasharray: 2, 85.964;
+        stroke-dashoffset: -65.973;
+        -webkit-transform: rotate(90deg);
+                transform: rotate(90deg);
       }
     }
   }
