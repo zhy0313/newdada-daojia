03ed735a839de421be3512f5445c6092fbbeefcd
Ivan <wcy5211678@163.com>
2017-04-18 10:53:39 +0800

    order

--- src/views/tabBar/OrderView.vue
+++ src/views/tabBar/OrderView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="">
-    <Loader></Loader>
+    <Loader v-if="loading"></Loader>
+    <div></div>
     <FooterNav :activeKey="3"></FooterNav>
   </div>
 
@@ -11,9 +12,43 @@
   import Loader from '@/components/Loader'
 
   export default {
+    data () {
+      return {
+        startIndex: 0,
+        orderlist: [],
+        loading: true
+      }
+    },
     components: {
       FooterNav,
       Loader
+    },
+    methods: {
+      fetchOrderList () {
+        this.$getAPI({
+          functionId: 'order/list',
+          body: {
+            startIndex: this.startIndex,
+            dataSize: 10
+          }
+        }).then((response) => {
+          if (response.body.code === '0') {
+            console.log('接口成功')
+            console.log('response', response.result)
+            this.orderlist.push(response.result)
+          } else {
+            console.log('接口失败', response.body)
+          }
+          this.loading = false
+          // this.$loading.toggle()
+          this.$toast({message: response.body.msg, position: 'center'})
+        }, (err) => {
+          console.log('err', err)
+        })
+      }
+    },
+    created () {
+      this.fetchOrderList()
     }
   }
 </script>
