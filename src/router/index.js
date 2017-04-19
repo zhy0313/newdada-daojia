afd8592d594c773b178a73293a8d2b2c2f034be4
langli <411280712@qq.com>
2017-04-19 11:07:47 +0800

    shouye

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,8 @@
 
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello' // 等同于 '../components/Hello'
-import Button from '@/components/Button'
-import SearchView from '@/views/SearchView'
-import CounterView from '@/views/CounterView'
-import SquareView from '@/views/SquareView'
-<<<<<<< HEAD
 import IndexView from '@/views/IndexView'
-=======
 import AddressView from '@/views/AddressView'
->>>>>>> e5990803cbc7bd67c2b3ed38485f8f7dc7773abd
 
 Vue.use(Router)
 
@@ -22,31 +14,6 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: IndexView
-    },
-    {
-      path: '/counter',
-      name: 'CounterView',
-      component: CounterView
-    },
-    {
-      path: '/search',
-      name: 'SearchView',
-      component: SearchView
-    },
-    {
-      path: '/square',
-      name: 'SquareView',
-      component: SquareView
-    },
-    {
-      path: '/hello',
-      name: 'Hello',
-      component: Hello
-    },
-    {
-      path: '/btn',
-      name: 'Button',
-      component: Button
     },
     {
       path: '/address',
