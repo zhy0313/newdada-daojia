e8ad5bc8ebe370d269ca7e2ac1d78c615a2f97c8
zhanglei03 <zhanglei03@imdada.cn>
2017-04-20 17:32:22 +0800

    settlementView

--- src/views/Cashier/CashierView.vue
+++ src/views/Cashier/CashierView.vue
@@ -38,13 +38,12 @@ export default {
       stationName: '',
       payType: 'wx',
       amount: '0.0',
-      countDownTime: '1798475',
+      countDownTime: '',
       checkedIndex: 0
     }
   },
   created () {
-    this.token = this.$route.query.token || 'N4Rm2EgrNoTbGgAOi/9pbw5xECE/X9bOwPI5vJ/JI9+UDeyMr0S2r9OebciNTP9e8yxfpNI11SBK2mWY42By/+PaXNqnZXOUrhpxO3/gfXP5p6mml7JZqIFL22nCtAbnUj2WG2rWkNkfJPFuuUr+OF++jwNAyKxUdtSPHhuiLwg='
-    this.userLogin()
+    this.token = this.$route.query.token || 's6MPyAYZ192mQWqRVsp0JVvwRBWKH+By4o2IRxR/aQZTT7D4BYQjFfV31jetkb0I2eM8T+c0JP3bSN+r9EgqWM2faJ12OD59CQsisktd5YffZR4s/IjnryOiftH+YIYXo+F7P4Rd61OwFWhTgKt51VOJBP0Wrj8KaITFtaU/3Ww='
     this.initCashier()
   },
   components: {
@@ -55,16 +54,6 @@ export default {
       this.checkedIndex = e.target.dataset.index
       this.payType = e.target.dataset.paytype
     },
-    userLogin: function () {
-      this.$getAPI({
-        functionId: 'login/testLogin',
-        body: {jdPin: 'cheung1984'}
-      }).then((response) => {
-        console.log(response)
-      }, (err) => {
-        console.log('err', err)
-      })
-    },
     initCashier: function () {
       this.$postAPI({
         functionId: 'cashier/initCashierPageInfo',
@@ -74,8 +63,8 @@ export default {
       }).then((response) => {
         console.log(response)
         this.getPayStatus()
-        // let result = response.body.result
-        let result = {'orderId': 705835766000014, 'amount': 24.20, 'notifyUrl': 'http://testpdjm.jd.com/html/index.html#orderopresult/result:success/payType:pay/orderId:705835766000014', 'payWays': [{'payWay': '10', 'payName': '微信支付', 'status': '0', 'defaultCopy': '推荐安装微信5.0及以上版本使用', 'iconUrl': 'http://storage.jd.com/daojia/wxpay-icon.png'}, {'payWay': '20', 'payName': '京东支付', 'status': '0', 'defaultCopy': '京东旗下 快捷支付', 'extCopy': '本订单支持使用白条', 'iconUrl': 'http://storage.jd.com/daojia/icon-jdpay.png', 'activityCopy': '京东支付首单满29最高减10（限实物类银行卡支付）'}], 'countDownTime': '1798475', 'stationName': '华冠-亦庄店', 'prescriptionTag': false}
+        let result = response.body.result
+        // let result = {'orderId': 705835766000014, 'amount': 24.20, 'notifyUrl': 'http://testpdjm.jd.com/html/index.html#orderopresult/result:success/payType:pay/orderId:705835766000014', 'payWays': [{'payWay': '10', 'payName': '微信支付', 'status': '0', 'defaultCopy': '推荐安装微信5.0及以上版本使用', 'iconUrl': 'http://storage.jd.com/daojia/wxpay-icon.png'}, {'payWay': '20', 'payName': '京东支付', 'status': '0', 'defaultCopy': '京东旗下 快捷支付', 'extCopy': '本订单支持使用白条', 'iconUrl': 'http://storage.jd.com/daojia/icon-jdpay.png', 'activityCopy': '京东支付首单满29最高减10（限实物类银行卡支付）'}], 'countDownTime': '1798475', 'stationName': '华冠-亦庄店', 'prescriptionTag': false}
         this.payWays = result.payWays
         this.stationName = result.stationName
         this.amount = result.amount
